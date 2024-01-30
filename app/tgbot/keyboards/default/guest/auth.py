2153d0929c323a15b43e03157983c6753d8a22b4
Dima Kacevich <d.katsevich.4@gmail.com>
2024-01-30 11:05:52 +0200

    Update keyboards

--- app/tgbot/keyboards/default/guest/auth.py
+++ app/tgbot/keyboards/default/guest/auth.py
@@ -2,19 +2,18 @@ from keyboards.default.consts import DefaultConstructor
 from texts.keyboards import START_FULL_REGISTRATION, GO_BACK
 
 guest_menu_text = {
-    "repairs": "Ремонтні роботи ⚒️️",
+    # "repairs": "Ремонтні роботи ⚒️️",
     "change_info": "Змінити дані 🔄",
     "share_chatbot": "Поділитися чат-ботом 🤝️",
     "reference_info": "Довідкова інформація ℹ️",
-    "review_enterprises": "Оцінити роботу підприємств міста ⭐",
+    # "review_enterprises": "Оцінити роботу підприємств міста ⭐",
     "full_registration": "Пройти повну реєстрацію ✍",
 }
 
-
 subscription_menu_kb = DefaultConstructor.create_kb(
-    actions=[item for item in guest_menu_text.values()], schema=[2, 2, 2]
+    actions=[item for item in guest_menu_text.values()], schema=[2, 2]
+    # actions=[item for item in guest_menu_text.values()], schema=[2, 2, 2]
 )
-
 
 full_registration_kb = DefaultConstructor.create_kb(
     actions=[START_FULL_REGISTRATION, GO_BACK], schema=[2]
--- app/viber/keyboards/cabinet.py
+++ app/viber/keyboards/cabinet.py
@@ -15,7 +15,7 @@ guest_menu_text = {
 
 guest_menu_kb = KeyboardConstructor.generate_kb(
     [
-        {"Text": guest_menu_text["repairs"], "Columns": 3, "Rows": 1},
+        # {"Text": guest_menu_text["repairs"], "Columns": 3, "Rows": 1},
         {"Text": guest_menu_text["change_info"], "Columns": 3, "Rows": 1},
         {
             "Text": guest_menu_text["share_chatbot"],
@@ -24,7 +24,7 @@ guest_menu_kb = KeyboardConstructor.generate_kb(
             "Columns": 3,
         },
         {"Text": guest_menu_text["reference_info"], "Columns": 3, "Rows": 1},
-        {"Text": guest_menu_text["review_enterprises"], "Columns": 3, "Rows": 1},
+        # {"Text": guest_menu_text["review_enterprises"], "Columns": 3, "Rows": 1},
         {"Text": guest_menu_text["full_registration"], "Columns": 3, "Rows": 1},
     ],
     {"InputFieldState": "hidden"},
@@ -57,7 +57,7 @@ cabinet_menu_kb = KeyboardConstructor.generate_kb(
         {"Text": cabinet_menu_text["create_request"], "Columns": 3, "Rows": 1},
         {"Text": cabinet_menu_text["actual_requests"], "Columns": 3, "Rows": 1},
         {"Text": cabinet_menu_text["history_requests"], "Columns": 3, "Rows": 1},
-        {"Text": cabinet_menu_text["repairs"], "Columns": 3, "Rows": 1},
+        # {"Text": cabinet_menu_text["repairs"], "Columns": 3, "Rows": 1},
         {"Text": cabinet_menu_text["change_user_info"], "Columns": 3, "Rows": 1},
         {
             "Text": cabinet_menu_text["share_chatbot"],
@@ -67,7 +67,7 @@ cabinet_menu_kb = KeyboardConstructor.generate_kb(
         },
         {"Text": cabinet_menu_text["reference_info"], "Columns": 3, "Rows": 1},
         {"Text": cabinet_menu_text["report_issue"], "Columns": 3, "Rows": 1},
-        {"Text": cabinet_menu_text["review_enterprises"], "Columns": 6, "Rows": 1},
+        # {"Text": cabinet_menu_text["review_enterprises"], "Columns": 6, "Rows": 1},
     ],
     {"InputFieldState": "hidden"},
 )
